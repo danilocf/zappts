9cb7b970d531379cb8c33d27f578a12c8af94b51
Danilo Freire <danilo.cf.sjc@gmail.com>
2020-04-07 14:34:43 -0300

    update PageNotFound

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,3 +1,12 @@
 <template>
-  <div class="PageNotFound"></div>
+  <div class="PageNotFound">
+    <h1>404. Page not found :(</h1>
+    <router-link :to="{ name: 'Index' }">Voltar para Home</router-link>
+  </div>
 </template>
+
+<style lang="scss">
+.PageNotFound {
+  text-align: center;
+}
+</style>
