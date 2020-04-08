107acb5fb6ca59e310fdaccb10c81e6f282473eb
Danilo Freire <danilo.cf.sjc@gmail.com>
2020-04-08 12:19:39 -0300

    add test e2e

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,20 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
+describe("everything", () => {
+  it("sign up", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.contains("Create Account").click();
+    cy.get("#fullname").type("test full name");
+    cy.get("#user").type("test user");
+    cy.get("#password").type("test password");
+    cy.contains("Sign up").click();
+  });
+
+  it("sign in", () => {
+    cy.visit("/");
+    cy.get("#user").type("test user");
+    cy.get("#password").type("test password");
+    cy.contains("Sign in").click();
+    cy.contains("Success! You are now logged in");
   });
 });
